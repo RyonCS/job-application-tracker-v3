568077b400159a1806be538d55a46116526cef91
Ryon Sajnovsky <111393217+RyonCS@users.noreply.github.com>
2025-07-22 13:59:05 -0400

    changed cors

--- Backend/src/controllers/auth-controller.ts
+++ Backend/src/controllers/auth-controller.ts
@@ -39,7 +39,7 @@ export const register = async (req: Request, res: Response) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'none',
       maxAge: 2 * 60 * 60 * 1000
     });
 
@@ -83,7 +83,7 @@ export const login = async (req: Request, res: Response) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'none',
       maxAge: 2 * 60 * 60 * 1000
     });
 
