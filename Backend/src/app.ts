997c161cc92b4416345179582c82d29d757938ee
Ryon Sajnovsky <111393217+RyonCS@users.noreply.github.com>
2025-08-19 14:46:11 -0400

    test

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -12,28 +12,14 @@ dotenv.config();
 // Create an instance of Express application.
 const app = express();
 
+// ===== CORS =====
+// CORS: open everything (debug)
+app.use(cors());           
+app.options('*', cors());   
+
 app.set('trust proxy', 1);
-
-app.use(cookieParser());
-
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL, 
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  })
-);
-
-// -- Middleware Setup: --
-
-// Parse URL-encoded bodies (form submission).
 app.use(express.urlencoded({ extended: true }));
-
-// Parse incoming JSON payloads.
 app.use(express.json());
-
-// Enable HTTP method override for supporting "PUT/DELETE" in forms.
 app.use(methodOverride('_method'));
 
 // -- Mount route handlers for different app sections: --
