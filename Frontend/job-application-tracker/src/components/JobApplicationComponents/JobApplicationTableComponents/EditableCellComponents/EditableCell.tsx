a92c3ad10aab39538c3f3f1be32912ba3bb84d66
Ryon Sajnovsky <111393217+RyonCS@users.noreply.github.com>
2025-08-20 09:49:02 -0400

    test

--- Frontend/job-application-tracker/src/components/JobApplicationComponents/JobApplicationTableComponents/EditableCellComponents/EditableCell.tsx
+++ Frontend/job-application-tracker/src/components/JobApplicationComponents/JobApplicationTableComponents/EditableCellComponents/EditableCell.tsx
@@ -53,6 +53,17 @@ const EditableCell = ({ field, value, onUpdate }: Props) => {
           finishEditing={finishEditing}
         />
       );
+    } else if (field === "linkToJobPosting" && tempValue) {
+      return (
+        <a
+          href={String(tempValue)}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-600 underline"
+        >
+          {String(tempValue)}
+        </a>
+      );
     }
 
     return (
