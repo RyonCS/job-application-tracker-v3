b70bcce5e4a3d6c4ee490c321737d1389d79c182
Ryon Sajnovsky <111393217+RyonCS@users.noreply.github.com>
2025-08-20 09:38:50 -0400

    test

--- Frontend/job-application-tracker/src/components/JobApplicationComponents/ApplicationSummaryCard.tsx
+++ Frontend/job-application-tracker/src/components/JobApplicationComponents/ApplicationSummaryCard.tsx
@@ -43,7 +43,7 @@ const ApplicationSummaryCard = ({cardHeaderText, summaryType}: Props) => {
         }
       };
       fetchSummary();
-    }, 500);
+    }, 250);
     
     return () => clearTimeout(timer);
   }, [jobApplications]);
